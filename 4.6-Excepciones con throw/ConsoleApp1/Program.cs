ba97c5db3431694ffb4a55b386e83be02301d2bf
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-03-13 23:35:27 -0300

    cola gorda 2.0

---
+++ 4.6-Excepciones con throw/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace usoDeThrow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ejemplo de los checked cortos
+            int Numero = int.MaxValue;
+            try
+            {
+                int resultado = checked(Numero + 20);// podemos usar el checked solo para una linea de este modo
+                int resultadoUnchecked = unchecked(Numero + 20); // tambien podemos pedir que cierta linea no maneje su excepcion
+                Console.WriteLine(resultado);
+
+            } catch (OverflowException ex)
+            {   
+                Console.WriteLine(ex.Message);
+            }
+
+            // ejemplo del uso de throw
+            try
+            {
+                Console.WriteLine("Introduce numero de mes: ");
+                int MesSoli = int.Parse(Console.ReadLine());
+                Console.WriteLine(obtenerMes(MesSoli));
+            } catch (FormatException ex)
+            {
+                Console.WriteLine("NO INGRESO UN NUMERO VALIDO");
+                Console.WriteLine("activaste el error pibe -> " + ex.Message);
+            }
+            
+        }
+
+
+
+        static string obtenerMes(int numMes)
+        {
+            // uso tipico de un switch
+            switch (numMes)
+            {
+                case 1:
+                    return "Enero";
+                case 2:
+                    return "Febrero";
+                case 3:
+                    return "Marzo";
+                case 4:
+                    return "Abril";
+                case 5:
+                    return "Mayo";
+                case 6:
+                    return "Junio";
+                case 7:
+                    return "Julio";
+                case 8:
+                    return "Agosto";
+                case 9:
+                    return "Septiembre";
+                case 10:
+                    return "Octubre";
+                case 11:
+                    return "Noviembre";
+                case 12:
+                    return "Diciembre";
+                default:
+                    throw new ArgumentOutOfRangeException(); // throw obliga a lanzar una excepcion especificada
+            }
+            
+            }       
+        }
+    
+    
+    }
+
+
