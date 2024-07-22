908b2847bf19f3689073ccfc116be6d19d543812
Francisco Monasterolo <monasterolofrancisco@gmail.com>
2024-07-21 21:35:46 -0300

    hilos mas HILOS

---
+++ 19-Hilos/hilos3- completacion de tareas/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+using System.Threading.Tasks;
+
+namespace Task
+{
+
+    // esta parte no esta del todo bien asi que puede tener errores en la explicacion
+
+    // basicamente si queremos que un hilo se ejecute al finalizar otro, podemos usar
+    // TaskCompletionSource para tener una forma de indicar esto a otros hilos
+
+    // tan como se realiza en el main, tenemos un objeto del tipo TaskCompletionSource
+    // el cual solicita como generico el tipo que la tarea devolvera cuando finalice
+    class Program
+    {
+       
+        static void Main(string[] args) {
+
+            var tareaTerminada = new TaskCompletionSource<bool>();
+
+            Thread hilo1 = new Thread(() =>
+            {
+                Thread.CurrentThread.Name = "hilo1";
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + $" {i}");
+                    Thread.Sleep(50);
+                }
+
+                // indicamos que la tarea termino al darle un resultado
+                tareaTerminada.SetResult(true);
+
+            });
+
+            Thread hilo2 = new Thread(() =>
+            {
+                Thread.CurrentThread.Name = "hilo2";
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + $" {i}");
+                    Thread.Sleep(50);
+                }
+
+
+            });
+
+            Thread hilo3 = new Thread(() =>
+            {
+                Thread.CurrentThread.Name = "hilo3";
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + $" {i}");
+                    Thread.Sleep(50);
+                }
+
+            });
+
+            // se ve que solo se ejecutara este hasta que task de un resultado
+
+            // es una forma de sincronizar 
+            hilo1.Start();
+
+            // cuando termine el hilo1, se podra recibir entonces el resultado 
+            // en la variable resultadoDeLaTarea accediendo a su propiedade Task.Result
+            var resultadoDeLaTarea = tareaTerminada.Task.Result;
+
+
+            // entonces podran empezar los siguientes hilos, que ahora se ejecutaran de forma
+            // asyncrona 
+            hilo2.Start();
+
+            hilo3.Start();  
+
+        }
+    }
+
+
+
+}
---
+++ 19-Hilos/hilos4- IMPORTANTE pools de hilos/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+// los thread pools son grupos de threads que se reparten tareas
+// de modo que no exista un thread por tarea y ahorrar recursos al equipo
+// siendo reutilizados para otra tarea una vez terminan una, de este modo 
+// se ahorra la sobrecarga de eliminarlos y crearlos constantemente
+
+// entonces optimizamos la cantidad de threads para multiples tareas repetitivas 
+// que requieren muchos threads activos, eso si
+
+// puede ser mas lento si se requiere de muchas tareas, pero ahorra mas recursos
+namespace ThreadPools
+{
+    // la cantidad de hilos que se encuentran en el pool esta determinado por el sistema
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 100; i++) {
+
+                // cuando creamos una tarea mas desde el bucle, QueueUserWorkItem
+                // pone en fila la tarea hasta que se libere uno de los threads del pool
+                // cuando esto suceda se ejecutara la tarea
+
+                // el QueueUserWorkItem admite que la tarea tenga un parametro 
+                // de tipo Object que podemos pasarle como lo hacemos con I
+
+                // de este modo varias tareas pueden trabajar sobre un dato distinto
+                ThreadPool.QueueUserWorkItem(TareaRepetitiva, i);
+            
+            
+            }
+            Console.ReadLine();
+
+        }
+
+
+        // el objeto objetoAlmacen servira para pasar a las tareas datos distintos de cualquier tipo
+        static void TareaRepetitiva(Object objetoAlmacen)
+        {
+
+            int NTarea = (int)objetoAlmacen;
+
+            Random rand = new Random();
+            int numeroAleatorio = rand.Next(1, 1000);
+
+            Console.WriteLine($"hilo {Thread.CurrentThread.ManagedThreadId} con tarea {NTarea} obtuvo: " + (numeroAleatorio * 4));
+
+        }
+
+    }
+
+
+
+}
